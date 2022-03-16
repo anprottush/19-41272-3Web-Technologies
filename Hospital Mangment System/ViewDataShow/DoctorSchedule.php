613758944ada9f9b58af8345fd420fac8b3d496c
anprottush <93096136+anprottush@users.noreply.github.com>
2022-03-16 06:58:11 +0600

    Add files via upload

---
+++ Hospital Mangment System/ViewDataShow/DoctorSchedule.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor Schedule</title>
+</head>
+<body>
+<?php
+    include('Header.php');
+?>
+    <h1>Doctor Schedule</h1>
+    <fieldset>
+        <legend>Doctor Information</legend>
+        <form action="../ControllerCheck/DoctorInformationCheck.php" method="POST" novalidate>
+        <div class="">
+        <label for=""  id="">Search Information:</label><br>
+        <input type="text" name="id" id="" placeholder="" required>
+      </div><br>
+
+      <div class="searchbutton">
+        <input type="submit" name="submit"id="submit" value="Search">
+      </div><br>
+    </form>
+    </fieldset>
+</body>
+</html>
+<?php
+    include('footer.php');
+    ?>
