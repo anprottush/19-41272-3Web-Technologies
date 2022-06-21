a75e0f9334146729055b55aa1a9105d5002ab72f
anprottush <93096136+anprottush@users.noreply.github.com>
2022-06-21 23:07:32 +0600

    Update Login.php

--- Hospital Mangment System/ViewDataShow/Login.php
+++ Hospital Mangment System/ViewDataShow/Login.php
@@ -1,3 +1,11 @@
+<?php
+include('Header.php');
+session_start();
+if(isset($_SESSION['username']))
+{
+    header("Location:../ViewDataShow/DashBoard.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
